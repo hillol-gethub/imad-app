8e6776495644ad7b4bc669dd1a96a740ad790dc5
hillol-gethub <30647821+hillol-gethub@users.noreply.github.com>
2017-08-13 21:32:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,10 @@ var tag = document.getElementById("main-id");
  
  // move the image
  var img = document.getElementById("madi");
- 
  var marginLeft = 0;
  function moveRight(){
      marginLeft = marginLeft + 1;
-     img.style.marginLeft = "marginLeft" + "px";
+     img.style.marginLeft = marginLeft + 'px';
      
  }
  
