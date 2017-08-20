8a995ac3f0241608bc4c0e15bd978fb237eeccd7
hillol-gethub <30647821+hillol-gethub@users.noreply.github.com>
2017-08-20 21:57:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,18 @@ app.get('/counter', function(req , res){
     counter = counter + 2;
     res.send(counter.toString());
     });
+    
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
