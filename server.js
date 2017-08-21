f66e575070ec1b7cb10b9e378100edbd9be9e928
hillol-gethub <30647821+hillol-gethub@users.noreply.github.com>
2017-08-21 22:40:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Poll = require('pg').Pool;
+
+var config = {
+    user: 'majumdar123hillol',
+    database: 'majumdar123hillol',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res){
+    pool.query('select * from test', function(err, result){
+       if(err){res.status(500).send(err.toString()) ;
+       else {res.send(JSON.stringify(result)); }
+    });
 });
 
 var counter = 0;
