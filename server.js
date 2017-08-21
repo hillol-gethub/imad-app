6b19c07138be5938ae7dbdbb840b1ed6d279748d
hillol-gethub <30647821+hillol-gethub@users.noreply.github.com>
2017-08-21 22:42:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
 var pool = new Pool(config);
 app.get('/test-db', function(req, res){
     pool.query('select * from test', function(err, result){
-       if(err){res.status(500).send(err.toString()) ;
+       if(err){res.status(500).send(err.toString()) ;}
        else {res.send(JSON.stringify(result)); }
     });
 });
