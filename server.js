70743d0ab7c35a5e9333d9a6cf2c3a2b9594d773
hillol-gethub <30647821+hillol-gethub@users.noreply.github.com>
2017-08-25 12:33:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,28 +3,67 @@ var morgan = require('morgan');
 var path = require('path');
 var Poll = require('pg').Pool;
 
-var config = {
+/*var config = {
     user: 'majumdar123hillol',
     database: 'majumdar123hillol',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
-};
+}; */
 
 var app = express();
 app.use(morgan('combined'));
+
+var articleOne = {
+	
+       title: 'Article One | Hillol Majumdar',
+       heading: 'Article',
+       date:'Sep 5, 2016',
+       content:`<p>
+       This is hillol. 1st article is displayed here.
+       </p>`
+    };
+    function createTemplete(data){
+       var title = data.title;
+       var date = data.date;
+       var heading = data.heading;
+       var content = data.content;
+ var htmlTemplate = `<html>
+  <head>
+	${title}
+	<meta name="viewport" content="width-device-width, initial-scale-1" />
+	<link href="/ui/style.css" rel="stylesheet" />
+  </head>
+  <body>
+      <div class='container'>
+    <div >
+      <a href="/">Home</a>
+    </div>
+    <hr/>
+    ${heading}
+    <div>
+      ${date}
+    </div>
+    <div>
+    ${content}
+   </div>
+   </div>
+  </body>
+</html>`;
+return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Poll(config);
+/*(var pool = new Poll(config);
 app.get('/test-db', function(req, res){
     pool.query('select * from test', function(err, result){
        if(err){res.status(500).send(err.toString());}
        else {res.send(JSON.stringify(result.rows)); }
     });
-});
+});*/
 
 var counter = 0;
 app.get('/counter', function(req , res){
@@ -33,7 +72,7 @@ app.get('/counter', function(req , res){
     });
     
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplete(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
