411ef864ad447b9c6c58bd95bf9c6820d5c8a07e
hillol-gethub <30647821+hillol-gethub@users.noreply.github.com>
2017-08-25 15:57:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,19 @@
-
+//console.log('Loaded!');
+/*var tag = document.getElementById("main-id");
+ tag.innerHTML = "helo its over";
+ 
+ // move the image
+ var img = document.getElementById("madi");
+ var marginLeft = 0;
+ function moveRight(){
+     marginLeft = marginLeft + 1;
+     img.style.marginLeft = marginLeft + 'px';
+     
+ }
+ 
+ img.onclick = function(){
+     var interval = setInterval(moveRight,100);
+};*/
 
 
 var button = document.getElementById("counter");
