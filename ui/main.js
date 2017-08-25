3cd5774b0c3aefb4de9ac0fe51fd52aafc7af0c0
hillol-gethub <30647821+hillol-gethub@users.noreply.github.com>
2017-08-25 15:49:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,24 @@
 var button = document.getElementById("counter");
 var counter = 0;
 button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
-}
+    
+    //create a request object
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some actrion
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = docuemnt.getElementById("count");
+                 span.innerHTML = counter.toString();
+            }
+        }
+        
+        
+    };
+    
+    //RENDER THE VARIABLE IN THE CORRECT SPAN
+    request.send('GET', 'http://majumdar123hillol.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
  
