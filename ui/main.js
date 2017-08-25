5fef6e84444d5b39924a755096ae61d6300a4c5f
hillol-gethub <30647821+hillol-gethub@users.noreply.github.com>
2017-08-25 19:02:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,7 +57,7 @@ submit.onclick = function(){
             //Take some actrion
             if(request.status === 200){
                 var names = request.responseText;
-                name = JSON.parse(names);
+                names = JSON.parse(names);
                 var list = '';
                 for(var i=0; i< names.length; i++){
                     list += '<li>' + names[i] + '</li>' ;
