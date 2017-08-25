c58f28ce38e694e840cff11966835fbcfcc9e85b
hillol-gethub <30647821+hillol-gethub@users.noreply.github.com>
2017-08-25 13:04:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,7 +47,9 @@ var articles = {
        var content = data.content;
  var htmlTemplate = `<html>
   <head>
+  <title>
 	${title}
+	</title>
 	<meta name="viewport" content="width-device-width, initial-scale-1" />
 	<link href="/ui/style.css" rel="stylesheet" />
   </head>
