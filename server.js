40cd4dd35ba7edf99918ee463befbb6ed59ad485
hillol-gethub <30647821+hillol-gethub@users.noreply.github.com>
2017-08-27 09:09:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/articles/:articleName', function (req, res) {
     //articles[articleName] == {} content object for aricle one
     // Fetch data from database to display
    // pool.query("select * from article where title = '" + req.params.articleName + "'", function (err, result){
-   pool.query("select * from article where title = $2", [req.params.articleName], function (err, result){
+   pool.query("select * from article where title = $1", [req.params.articleName], function (err, result){
         if(err){res.status(500).send(err.toString());}
         else {
             if(result.rows.length === 0){
